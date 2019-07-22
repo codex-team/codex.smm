2f8da1db59691fc51efafb1bc43527ecb2db812c
Nikita Melnikov <nikmel2803@gmail.com>
2019-07-22 17:21:58 +0300

    move settings to the dotenv file

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ const Agent = require('socks5-https-client/lib/Agent');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+require('dotenv').config();
+
 const agent = new Agent({
-  socksHost: '50.62.59.61',
-  socksPort: '62705'
+  socksHost: process.env.SOCKS_HOST,
+  socksPort: process.env.SOCKS_PORT
 });
 
 async function main() {
-  const response = await axios.get('https://t.me/s/codex_team', {
+  const response = await axios.get(process.env.CHANNEL_TO_LOOKUP_URL, {
     httpsAgent: agent
   });
   const $ = cheerio.load(response.data);
   const lastPostTimestamp = $('time').last().attr('datetime');
   const date = new Date(lastPostTimestamp);
   const currentDate = new Date();
+  const daysWithoutPosts = 1000 * 60 * 60 * 24 * process.env.DAYS_WITHOUT_POSTS;
 
-  if (currentDate - date > 1000 * 60 * 60 * 24 * 7) {
+  if (currentDate - date > daysWithoutPosts) {
     await axios({
       method: 'post',
-      url: '',
-      data: 'message=Hello world'
+      url: process.env.NOTIFY_URL,
+      data: 'message=И почему так долго не было постов?'
     });
   }
 }
