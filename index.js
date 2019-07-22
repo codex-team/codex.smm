87158ca98866a9792c779acc53db5b1821ac2510
Nikita Melnikov <nikmel2803@gmail.com>
2019-07-22 16:20:02 +0300

    send request via axios

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const shttps = require('socks5-https-client');
+const Agent = require('socks5-https-client/lib/Agent');
+const axios = require('axios');
 
-shttps.get({
-    hostname: 't.me',
-    path: '/s/codex_team',
-    rejectUnauthorized: false,
+const agent = new Agent({
     socksHost: '50.62.59.61',
-    socksPort: '62705',
-}, function(res) {
-    res.setEncoding('utf8');
-    res.on('readable', function() {
-        console.log(res.read()); // Log response to console.
-    });
-});
+    socksPort: '62705'
+})
+
+async function main() {
+    const response = await axios.get('https://t.me/s/codex_team',
+        {
+            httpsAgent: agent
+        });
+    console.log(response.data)
+}
+
+main();
